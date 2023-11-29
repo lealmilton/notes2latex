ac02a621bd978fa3801022fff79406f31761fa83
lealmilton <leal.milton@gmail.com>
2023-11-29 07:20:55 -0300

    increases max output tokens

--- src/text_processing/reasoning.py
+++ src/text_processing/reasoning.py
@@ -4,10 +4,11 @@ def gpt4_completion(context, file_content, prompt):
     client = OpenAI()  # Initialize the OpenAI client
     full_prompt = context + file_content + "\n\n" + prompt
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4-1106-preview", 
         messages=[
             {"role": "system", "content": "You are a math editor."},
             {"role": "user", "content": full_prompt}
-        ]
+        ],
+        max_tokens=4096,
     )
     return completion.choices[0].message.content
