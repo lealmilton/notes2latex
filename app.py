028a21e6924ae760e5bd138901a406e8110f795d
lealmilton <leal.milton@gmail.com>
2023-11-30 07:58:17 -0300

    fixes states

--- app.py
+++ app.py
@@ -23,47 +23,47 @@ st.subheader("📝 Convert Handwritten Math to LaTeX using AI 📄")
 st.write("As of now, please only upload PDFs of up to 3 pages.")
 st.write("Longer files have increased error rates and take a long time to generate.")
 
+# Only process new file uploads
 uploaded_file = st.file_uploader("Upload your PDF (up to 3 pages)", type="pdf")
-
-# Create the progress bar only once after the file is uploaded
-if uploaded_file is not None and not st.session_state['progress_bar_active']:
-    st.session_state['progress_bar'] = st.progress(0)
-    st.session_state['progress_bar_active'] = True
-
-if not st.session_state['processing_completed'] and uploaded_file is not None:
+if uploaded_file is not None and not st.session_state['processing_completed']:
     with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
         tmp.write(uploaded_file.getvalue())
         pdf_path = tmp.name
 
-    if pdf_path:
-        with st.spinner("Well, it's AI. It takes time. Gpus are running somewhere. Go read a theorem and come back soon 😂."):            # Convert PDF to PNG
-            png_output_folder = convert_pdf_to_png(pdf_path)
-            st.session_state['progress_bar'].progress(25)
+    # Create and update the progress bar
+    if 'progress_bar' not in st.session_state:
+        st.session_state['progress_bar'] = st.progress(0)
+        st.session_state['progress_bar_active'] = True
 
-            # Process PNG images with OCR
-            ocr_text_folder = process_png_images(png_output_folder)
-            st.session_state['progress_bar'].progress(50)
+    with st.spinner("Well, it's AI. It takes time. GPUs are running somewhere. Go read a theorem and come back soon 😂."):
+        # Convert PDF to PNG
+        png_output_folder = convert_pdf_to_png(pdf_path)
+        st.session_state['progress_bar'].progress(25)
 
-            # Concatenate text files
-            combined_text_path = concatenate_text_files(ocr_text_folder)
-            st.session_state['progress_bar'].progress(75)
+        # Process PNG images with OCR
+        ocr_text_folder = process_png_images(png_output_folder)
+        st.session_state['progress_bar'].progress(50)
 
-            # Create LaTeX from text
-            tex_path = create_tex(combined_text_path)
-            st.session_state['progress_bar'].progress(90)
+        # Concatenate text files
+        combined_text_path = concatenate_text_files(ocr_text_folder)
+        st.session_state['progress_bar'].progress(75)
 
-            # Output final PDF
-            output_file_path = output_pdf(tex_path)
-            st.session_state['progress_bar'].progress(100)
+        # Create LaTeX from text
+        tex_path = create_tex(combined_text_path)
+        st.session_state['progress_bar'].progress(90)
 
-            # Update session state
-            st.session_state['tex_path'] = tex_path
-            st.session_state['output_file_path'] = output_file_path
-            st.session_state['processing_completed'] = True
-            st.session_state['progress_bar_active'] = False  # Reset progress bar state
+        # Output final PDF
+        output_file_path = output_pdf(tex_path)
+        st.session_state['progress_bar'].progress(100)
 
-            # Clean up temporary files
-            os.unlink(pdf_path)
+        # Update session state
+        st.session_state['tex_path'] = tex_path
+        st.session_state['output_file_path'] = output_file_path
+        st.session_state['processing_completed'] = True
+        st.session_state['progress_bar_active'] = False  # Reset progress bar state
+
+        # Clean up temporary files
+        os.unlink(pdf_path)
 
 # Display download buttons if processing is completed
 if st.session_state['processing_completed']:
@@ -71,10 +71,10 @@ if st.session_state['processing_completed']:
     
     if st.session_state['tex_path']:
         with open(st.session_state['tex_path'], "r") as tex_file:
-            st.download_button("Download LaTeX Source", tex_file, "raw.tex")
+            st.download_button("Download LaTeX Source", tex_file.read(), "raw.tex")
 
     if st.session_state['output_file_path']:
         with open(st.session_state['output_file_path'], "rb") as file:
-            st.download_button("Download LaTeX PDF", file, "processed.pdf")
+            st.download_button("Download LaTeX PDF", file.read(), "processed.pdf")
 
 st.write("Thank you for using Notes2LaTeX!")
